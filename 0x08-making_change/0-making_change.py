38d66b099dc9bc9aa43fd6ba5c9765d44e75524a
titobzzz <olumoyinjoshua19609@gmail.com>
2024-07-24 13:43:24 +0100

    commit changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+question on fewest number of coins 
+to meet a given amount total
+"""
+
+
+def makeChange(coins, total):
+    """ fewest numberof coinsneeded to meettotal """
+    if total <= 0:
+        return 0
+    # sort the coins in descending order
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+    if total != 0:
+        return -1
+    return change
